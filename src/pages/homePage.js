802bfdbd29d822f100411d417565d8cd01d222d0
Thongbruh <91879343+accnaylaaccfake111@users.noreply.github.com>
2025-10-11 18:18:50 +0700

    change img path

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,8 +1,10 @@
+import { AlignCenter } from "lucide-react";
+
 function Home() {
     return(
         <div>
             <h1 style={{textAlign: 'center'}}>NGHE THUAT HAT SAC BUA PHU LE</h1>
-            <img src="D:\ADMIN\PROJECTX\KH25-26\WEBSITE\my-react-app\src\img\hat-sac-bua-1319.jpg"/>
+            <img src="img/hat-sac-bua-1319.jpg"></img>
         </div>
     );
 }
